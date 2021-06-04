96df698d3cc5b303ce9acd7ea77f609849e95bc9
liuchaochaoliu <50480370+liuchaochaoliu@users.noreply.github.com>
2021-06-04 20:39:31 +0800

    iterative dfs
    dfs was implemented through iterative approach, but it has issue with graphs containing bi-directional edges

--- graph_search.py
+++ graph_search.py
@@ -7,44 +7,79 @@ class dfs:
     by applying Kosaraju's two-pass algorithm.
     '''
     
-    def __init__(self, g):
-        self.g = g
-        g_heads = g.iloc[:, 1].unique().tolist()
-        g_tails = g.iloc[:, 0].unique().tolist()
+    def __init__(self, filename):
+        # import txt file
+        g_df = pd.read_csv(filename, sep=' ', header=None)
+        if len(g_df.columns) > 2:
+            g_df = g_df.drop(g_df.columns[2:], axis=1)
+        g_heads = g_df.iloc[:, 1].unique().tolist()
+        g_tails = g_df.iloc[:, 0].unique().tolist()
         self.vertices = np.unique(g_heads + g_tails).tolist()
         self.n = len(self.vertices)
-        self.m = len(g)    
+        self.m = len(g_df) 
+        print(f'there are {self.n} nodes and {self.m} edges in the loaded file')
+        
+        # prepare graph and reversed graph in the right formats
+        gr =[[] for i in range(self.n)]
+        r_gr = [[] for i in range(self.n)]
+        file = open(filename, 'r')
+        data = file.readlines()
+        for line in data:
+            if line != '\n':
+                items = line.split()
+                gr[int(items[0])-1] += [int(items[1])]
+                r_gr[int(items[1])-1] += [int(items[0])]
+        self.g = gr
+        self.g_rev = r_gr
+        
+        # initialize list of finishing times
         self.f = [np.nan] * self.n
 
-        g_rev = pd.concat([g.iloc[:, 1],  g.iloc[:, 0]], axis=1)
-        g_rev.columns = g.columns
-        self.g_rev = g_rev
+        print(self.g_rev)
     
 
-    def dfs_search(self, g, i):
+    def dfs_search(self, g):
         '''
         this function does the dfs search and update finishing times recursively
         args:
         -g  : graph 
-        -i  : initial node to launch dfs search
         '''
+        while self.stack:
+                    
+            print('stack initial reation')
+            print([item+1 for item in self.stack])
 
-        # set i as explored
-        self.exp_list[i] = True
-        # set i as leader
-        self.leader_list[i] = self.s
-        arcs_out = [item for item in g.values if i == item[0]-1]
-        # perform dfs search recursively
-        for item in arcs_out:
-            new_node = item[1] - 1
-            if not self.exp_list[new_node]:
-                self.dfs_search(g, new_node)
-        # update finishing time
-        self.t = self.t + 1
-        self.f[i] = self.t
+            i = self.stack[0] # first element in stack
+            # set i as explored
+            self.exp_list[i] = True
+            # set i as leader
+            self.leader_list[i] = self.s
+
+
+            # build stacks (firt in first out)
+            done = True
+            nbr_nodes = g[i]
+            for nbr_node in nbr_nodes:
+                new_node = nbr_node - 1
+                if not self.exp_list[new_node]:
+                    self.stack = [new_node] + self.stack
+                    self.exp_list[new_node] = True
+                    done = False
+                    print('stack creation')
+                    print([item+1 for item in self.stack])
+
+            # update finishing time
+            if done:
+                self.t = self.t + 1
+                self.f[i] = self.t
+                self.order = [i] + self.order
+                print('stack elimination:')
+                print([item+1 for item in self.stack])
+                print(f't = {self.t}')
+                del self.stack[0]
  
 
-    def dfs_loop(self, g, order='sequential'):
+    def dfs_loop(self, g, mode='sequential'):
         '''
         launch dfs search iteratively from unexplored nodes(leaders). It has two modes:
         1) sequantial mode: sequentially check if a node is explored based on their node index
@@ -52,31 +87,36 @@ class dfs:
 
         args:
         -g      : graph
-        -order  : mode of running dfs_loop
+        -mode  : mode of running dfs_loop
         '''
-        assert order in ['sequential', 'finishing_time'], print('specify correct order')
+        assert mode in ['sequential', 'finishing_time'], print('specify correct mode')
         
         # initialization
         self.exp_list = [False] * self.n    
         self.leader_list = [np.nan] * self.n
+        self.stack = []
         self.t = 0
+        self.order = []
         counter = 0  
         
-        if order == 'sequential':       
+        if mode == 'sequential':       
             for i in reversed(range(self.n)):
                 counter = counter + 1
-                print(f'node_{i} was processed. {counter} of {self.n} nodes have been processed')
+                print(f'node_{i+1} was processed. {counter} of {self.n} nodes have been processed')
                 if not self.exp_list[i]:
                     self.s = i + 1
-                    self.dfs_search(g, i)
-        elif order == 'finishing_time':
-            f_idx = np.argsort(-np.array(self.magical_order))
-            for node_idx in f_idx:
+                    self.stack = [i]
+                    self.dfs_search(g)
+
+        elif mode == 'finishing_time':
+            for node_idx in self.magical_order:
                 counter = counter + 1
-                print(f'node_{node_idx} was processed. {counter} of {self.n} nodes have been processed')
+                print(f'node_{node_idx+1} was processed. its finishing value is  {self.magical_order[node_idx]}.\
+                    {counter} of {self.n} nodes have been processed')
                 if not self.exp_list[node_idx]:
                     self.s = node_idx + 1
-                    self.dfs_search(g, node_idx)
+                    self.stack = [node_idx]
+                    self.dfs_search(g)
     
 
     def dfs_scc(self):
@@ -88,12 +128,14 @@ class dfs:
 
         # step 1: run dfs-loop on reversed graph
         self.dfs_loop(self.g_rev)
-        self.magical_order = self.f.copy()
+        self.magical_order = self.order.copy()
         print(f'1st pass of dfs-loop on reversed graph was successfully completed')
+        print(f'finishing values are {self.f}')
+        print(f'magical orders are {[item+1 for item in self.magical_order]}')
 
         # step 2: run dfs-loop on graph
         assert np.nan not in self.f, print('not every nodes has finishing time')
-        self.dfs_loop(self.g, order='finishing_time')
+        self.dfs_loop(self.g, mode='finishing_time')
         print(f'2nd pass of dfs-loop on the actual graph was successfully completed')
 
         # step 3: group the nodes by SCC
--- main.py
+++ main.py
@@ -5,18 +5,13 @@ import argparse
 import time
 from datetime import datetime, timedelta
 
-def run_dfs_exp(data_file_path):
-	
-	# 1.0 import graph from txt file
-	g_df = pd.read_csv(data_file_path,  sep=' ', header=None)
-	if len(g_df.columns) > 2:
-		g_df = g_df.drop(g_df.columns[2:], axis=1)
+def run_dfs_exp(filename):
 
-	# 2.0 run dfs search to find scc regions
-	dfs_engine = dfs(g_df)
+	# 1.0 run dfs search to find scc regions
+	dfs_engine = dfs(filename)
 	scc_df = dfs_engine.dfs_scc()
 
-	# 3.0 print out the largest 5 scc regions
+	# 2.0 print out the largest 5 scc regions
 	scc_sizes = -np.sort(-scc_df.groupby('leader').size())
 	if len(scc_sizes) >=5:
 	    print(f'The largest 5 scc sizes are {scc_sizes[:5]}')
